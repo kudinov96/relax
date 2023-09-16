90fcf7cc54bda486432ab0462cd0802bba2c3639
kudinov96 <ilya.kudinov96@yandex.ru>
2023-09-16 17:00:06 +0300

    add payment methods

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,5 +2,9 @@
     <div class="container">
         <p>RelaxTime 2023  &copy; <?php date("Y"); ?></p>
         <p><a href="https://relaxtime.lv/privacy-policy/" target="_blank">{{ __("Политика конфиденциальности") }}</a></p>
+        <div class="payment-methods">
+            <img src="https://relaxtime.lv/wp-content/themes/understrap/images/visa.svg" width="91" height="31" alt="" style="margin-right: 25px">
+            <img src="https://relaxtime.lv/wp-content/themes/understrap/images/mastercard.svg" width="66" height="43" alt="">
+        </div>
     </div>
 </footer>
